89232c5ac99d618c6a0b02a27c287cf39333b8a2
Jandroo <125157604+JandroBv@users.noreply.github.com>
2023-03-27 11:44:38 -0600

    Update IDA.py

--- IDA.py
+++ IDA.py
@@ -6,10 +6,7 @@ class Pancakes:
 	def __init__(self, pancakes):
 		self.pancakes = pancakes
 		self.orden = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"]
-		self.profundidad = 1
-		self.valor = self.h(pancakes)
-		self.min = math.inf
-		self.cota = self.valor
+		self.solucion = ""
 	
 	def voltear(self, posicion, pancakes):
 		return pancakes[0:len(pancakes)-posicion] + pancakes[len(pancakes)-posicion:len(pancakes)][::-1]
@@ -22,43 +19,47 @@ class Pancakes:
 			if (abs(self.orden.index(nodo[i])-self.orden.index(nodo[i+1])) > 1):
 				c += 1
 		return c
-	
+
 	def ida(self, nodo):
 		if nodo == self.orden[0:len(nodo)]:
-			return
+			return 
+		cota = self.h(nodo)
 		while True:
-			resultado = self.busqProfundidad(nodo, ["0"], self.h(nodo))
-			if resultado == -1:
-				self.profundidad += 1
-			elif resultado == -2:
-				self.cota = self.min
-				self.min = math.inf
-			else:
-				return resultado
+			resultado = self.busqProfundidad(nodo, ["0"], cota)
+			if resultado == True:
+				return " ".join(self.solucion[1:])
+			
+			cota = resultado
 
-	def busqProfundidad(self, nodo, ant, h):
+
+	def busqProfundidad(self, nodo, ant, cota):
+
+		f = self.h(nodo) + len(ant) - 1
+
+		if f > cota:
+			return  f
+
 		if nodo == self.orden[0:len(nodo)]:
-			return " ".join(ant[1:])
-	
-		if len(ant)-1 >= self.profundidad:
-			return -1 
-		for i in range(2, len(nodo)+1):
-			if str(i) == ant[-1]:
-				continue 
-			nodo_volt = self.voltear(i,nodo)
-			if (self.h(nodo_volt) + 1) <= self.cota:
-					resultado = self.busqProfundidad(nodo_volt, ant + [str(i)], self.h(nodo_volt))
-					if resultado != -1:
-						break
-			else:
-				if (self.h(nodo_volt) + 1) < self.min:
-					self.min = self.h(nodo_volt) + 1
-				resultado = -2
-		return resultado
+			return True
+		
+		minimo = math.inf 
 
+		for i in range(2, len(nodo) + 1):
+			if i == ant[-1]:
+				continue
+			nodo_volt = self.voltear(i, nodo)
+			resultado = self.busqProfundidad(nodo_volt, ant + [str(i)], cota)
+			if resultado == True:
+				if self.solucion == "":
+					self.solucion = ant + [str(i)]
+				return True
+			if resultado < minimo:
+				minimo = resultado
 
-pancakes = ["a", "b", "c", "d","e", "f"]
+		return minimo
+
+pancakes = ["a", "b", "c", "d","e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"]
 random.shuffle(pancakes)
 print(f"Pancakes inicio: {pancakes}")
 prueba = Pancakes(pancakes)
-print(prueba.ida(pancakes))
+print(f"Solucion: {prueba.ida(pancakes)}")
